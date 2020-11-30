900416f395c3ef79460ff2d9653a17db831a93e0
Stenal P Jolly <sjolly@synamedia.com>
2020-11-30 15:05:52 +0530

    CodeMirror Based Editor

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,28 @@
 import * as React from "react";
-import { hot } from "react-hot-loader";
-
-const reactLogo = require("./../assets/img/react_logo.svg");
+import {hot} from "react-hot-loader";
 import "./../assets/scss/App.scss";
+import { UnControlled as CodeMirror} from "react-codemirror2";
+
+require('codemirror/mode/javascript/javascript');
 
 class App extends React.Component<Record<string, unknown>, undefined> {
-  public render() {
-    return (
-      <div className="app">
-        <h1>Hello World!</h1>
-        <p>Foo to the barz</p>
-        <img src={reactLogo.default} height="480" />
-      </div>
-    );
-  }
+    options: any = {
+        mode: 'javascript',
+        theme: 'ayu-mirage',
+        lineNumbers: true
+    };
+    state: any;
+    value: any;
+    public render() {
+        return (
+            <CodeMirror
+                value={this.value}
+                options={this.options}
+                onChange={(editor, data, value) => {
+                    console.log(editor, data)
+                }}/>
+        );
+    }
 }
 
 declare let module: Record<string, unknown>;
