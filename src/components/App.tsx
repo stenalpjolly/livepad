9c2a1c991cb121dd51b1dbdad47ed87f4c5e609d
Stenal P Jolly <sjolly@synamedia.com>
2020-12-01 17:42:20 +0530

    Adding peer to peer connection using peerjs

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,11 +5,13 @@ import {JoinSession} from "./JoinSession";
 
 import "./../assets/scss/App.scss";
 import {useEffect, useState} from "react";
+import Peer from "peerjs";
 
 require('codemirror/mode/javascript/javascript');
 
 function App(){
     const [value, setValue] = useState("");
+    const [conn, setConn] = useState<Peer.DataConnection>();
     const options: any = {
         mode: 'javascript',
         theme: 'ayu-mirage',
@@ -25,16 +27,24 @@ function App(){
         }
     });
 
+    if (conn && conn.open) {
+        conn.on("data", data => {
+            console.log(JSON.parse(data));
+        });
+    }
+
     return (
         <>
-            <JoinSession/>
+            <JoinSession setConnection={setConn}/>
             <CodeMirror
                 value={value}
                 options={options}
                 onChange={(editor, data, value) => {
                     localStorage.setItem("content", value);
+                    if (conn && conn.open) {
+                        conn.send(JSON.stringify(data));
+                    }
                 }}>
-
             </CodeMirror>
         </>
     );
--- src/components/JoinSession.tsx
+++ src/components/JoinSession.tsx
@@ -1,16 +1,37 @@
 import * as React from "react";
-import {useState} from "react";
+import {useCallback, useState} from "react";
 import {Button, FormControl, InputGroup, Modal} from "react-bootstrap";
+import Peer from "peerjs";
 
-export const JoinSession = () => {
+export const JoinSession = (props) => {
     const [show, setShow] = useState(true);
 
     const handleClose = () => setShow(false);
     const createSession = () => {
+        const peer = new Peer('synamedia-spj-sample-3');
+        peer.on('connection', connection => {
+            // connection.on("data", data => console.log("Data from peer is ", data))
+            props.setConnection(connection);
+        });
+        // peer.on('open', function(id) {
+        //     console.log('My peer ID is: ' + id);
+        // });
+
         handleClose();
     };
 
     const joinSession = () => {
+        const peer = new Peer();
+        peer.on('open', function(id) {
+            const connection = peer.connect('synamedia-spj-sample-3');
+            props.setConnection(connection);
+            // console.log('My peer ID is: ' + id);
+            // connection.on('open', () => {
+            //     console.log("Connection opened");
+            //     // conn.send('hi!');
+            // });
+            // connection.on("data", data => console.log(data));
+        });
         handleClose();
     };
 
