18c1766d96c63ea4ac5b1cd9cb853dc23fd04dd7
Stenal P Jolly <sjolly@synamedia.com>
2021-04-23 10:50:05 +0530

    Refactor and optimize user list display

--- src/components/StatusBar.tsx
+++ src/components/StatusBar.tsx
@@ -1,37 +1,36 @@
 import * as React from "react";
-import { Button, Nav, Navbar } from "react-bootstrap";
+import {Button, Nav, Navbar} from "react-bootstrap";
 
-export const StatusBar = (props: { userName: string[] }): JSX.Element => {
-  let navbar: JSX.Element = <></>;
-  if (props.userName && props.userName.length > 0) {
-    const getButton = () => {
-      const buttons = [];
-      for (let index = 0; index < props.userName.length; index++) {
-        buttons.push(
+const getButton = (props: { userList: string[] }) => {
+    const buttons = [];
+    for (let index = 0; props.userList && index < props.userList.length; index++) {
+      buttons.push(
           <Button
-            key={index}
-            size="sm"
-            variant="outline-info"
-            disabled
-            className="pull-left"
-            text-align="right"
+              key={index}
+              size="sm"
+              variant="outline-info"
+              disabled
+              className="pull-left"
+              text-align="right"
           >
-            {props.userName[index]}
+            {props.userList[index]}
           </Button>,
-        );
-        buttons.push(<>&nbsp;</>);
-      }
-      return buttons;
-    };
+      );
+      buttons.push(<>&nbsp;</>);
+    }
+    return buttons;
+};
+
+export const StatusBar = (props: { userList: string[] }): JSX.Element => {
+    let navbar: JSX.Element = <></>;
     navbar = (
-      <>
-        <Navbar className="fixedBar" bg="dark" fixed="bottom">
-          {" "}
-          {/*Bootstrap*/}
-          <Nav className="mr-auto right30 navbar-nav">{getButton()}</Nav>
-        </Navbar>
-      </>
+        <>
+            <Navbar className="fixedBar" bg="dark" fixed="bottom">
+                {" "}
+                {/*Bootstrap*/}
+                <Nav className="mr-auto right30 navbar-nav">{getButton(props)}</Nav>
+            </Navbar>
+        </>
     );
-  }
-  return navbar;
+    return navbar;
 };
