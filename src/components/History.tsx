1e245efe4565ed5364720927410aa01179579f9c
Stenal P Jolly <sjolly@synamedia.com>
2021-04-25 22:00:39 +0530

    Basic playback of history

--- src/components/History.tsx
+++ src/components/History.tsx
@@ -6,63 +6,71 @@ import {useEffect, useState} from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import * as icons from "@fortawesome/free-solid-svg-icons";
 
-let speed = 1;
+let currentSpeed = 1;
 let count = 0;
+let historyData: any;
 
-const startPlay = (setPr: (value: (((prevState: string) => string) | string)) => void, setData: (value: (((prevState: string) => string) | string)) => void, historyData: any) => {
+const startPlay = (setPr: (value: (((prevState: string) => string) | string)) => void, setData: (value: (((prevState: string) => string) | string)) => void) => {
     setInterval(function () {
-        moveNext(setData, setPr, historyData)
-    }, 300);
+        moveNext(setData, setPr, historyData, currentSpeed)
+    }, 350);
 }
 
-function moveNext(setData: (value: (((prevState: string) => string) | string)) => void, setPr: (value: (((prevState: string) => string) | string)) => void, historyData: any){
-    setData(historyData[count]);
-    count = count + speed;
-    count = count >= historyData.length ? 0 : count;
+function moveNext(setData: (value: (((prevState: string) => string) | string)) => void, setPr: (value: (((prevState: string) => string) | string)) => void, historyData: any, newSpeed: any){
+    let newData = historyData[count].split("\n")
+        .map((data,index) => {
+            data = data.replace(/\s/g, "&nbsp;")
+            return `<div class="font-1-rem align-content-center"><span class="line-number">${index}&nbsp;&nbsp;</span><span>${data}</span></div>`
+        })
+        .join("")
+    setData(newData);
+    count = count + newSpeed;
+    if (count < 0 || count >= historyData.length) {
+        count = 0;
+    }
     setPr( `${count / historyData.length * 100}%` );
 }
 
 export const HistoryComponent = () => {
 
-    const [pr, setPr] = useState("0%")
+    const [marginPercentage, setMarginPercentage] = useState("0%")
     const [data, setData] = useState("");
 
     const query: ParsedUrlQuery = queryString.decode(location.search, "?", "=");
     const sessionId = query.sessionId;
-    const historyData = getHistoryList(sessionId);
+    historyData = getHistoryList(sessionId);
 
     useEffect( () => {
-        startPlay(setPr, setData, historyData)
+        startPlay(setMarginPercentage, setData)
     }, []);
 
     return (
         <div className="history">
-            <div className="code-data ">
-                {data}
+            <div className="container code-data ">
+                <div dangerouslySetInnerHTML={{__html: data}}/>
             </div>
             <div className="panel">
                 <div className="background">
                     <div className="btns">
                         <FontAwesomeIcon color="#424350" size="lg" icon={icons.faFastBackward} onClick={()=>{
-                            speed -= 1;
-                            if (speed < 0) {
-                                speed = 0;
-                            }
+                            currentSpeed = 0;
+                            moveNext(setData, setMarginPercentage, historyData, -1);
                         }}/>
                         <FontAwesomeIcon color="#424350" size="lg" icon={icons.faPlay} onClick={()=>{
-                            speed = 1;
+                            currentSpeed = 1;
                         }}/>
                         <FontAwesomeIcon color="#424350" size="lg" icon={icons.faPause} onClick={()=>{
-                            speed = 0;
+                            currentSpeed = 0;
                         }}/>
                         <FontAwesomeIcon color="#424350" size="lg" icon={icons.faFastForward} onClick={()=>{
-                            speed += 1;
+                            currentSpeed = 0;
+                            moveNext(setData, setMarginPercentage, historyData, 1);
                         }}/>
                     </div>
                     <div className="slider">
                         <div className="slider-background">
                         </div>
-                        <div className="slider-foreground" style={{marginLeft: pr}}>
+                        <div className="slider-foreground" style={{marginLeft: marginPercentage}}>
                         </div>
                     </div>
                     <div className="counter">
