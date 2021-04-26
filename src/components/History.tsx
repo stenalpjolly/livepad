0a16e15d10934db7b12e8670dbebbe0e36d0b4d6
Stenal P Jolly <sjolly@synamedia.com>
2021-04-26 21:48:27 +0530

    [BugFix] Code replay with highlight and selection

--- src/components/History.tsx
+++ src/components/History.tsx
@@ -1,60 +1,72 @@
 import * as React from "react";
 import {useEffect, useState} from "react";
+import * as CodeMirror from "codemirror";
 import * as queryString from "querystring";
 import {ParsedUrlQuery} from "querystring";
 import {getHistoryList} from "../utils/LocalStore";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faPlay, faPause, faFastBackward, faFastForward} from "@fortawesome/free-solid-svg-icons";
+import {EditorConfiguration} from "codemirror";
 
 let currentSpeed = 1;
 let count = 0;
 let historyData: any;
+let timer;
+global.CodeMirror = CodeMirror;
 
-const startPlay = (setPr: (value: (((prevState: string) => string) | string)) => void, setData: (value: (((prevState: string) => string) | string)) => void) => {
-    setInterval(function () {
-        moveNext(setData, setPr, historyData, currentSpeed)
+let editor: CodeMirror.Editor;
+const options: EditorConfiguration = {
+    theme: "ayu-mirage",
+    readOnly: "nocursor",
+    lineWrapping: true,
+    lineNumbers: true,
+};
+
+const startPlay = (setPr: (value: (((prevState: string) => string) | string)) => void) => {
+    timer = setInterval(function () {
+        moveNext(setPr, historyData, currentSpeed)
     }, 350);
 }
 
-function moveNext(setData: (value: (((prevState: string) => string) | string)) => void, setPr: (value: (((prevState: string) => string) | string)) => void, historyData: any, newSpeed: any){
-    let newData = historyData[count].split("\n")
-        .map((data,index) => {
-            data = data.replace(/\s/g, "&nbsp;")
-            return `<div class="font-1-rem align-content-center"><span class="line-number">${index}&nbsp;&nbsp;</span><span>${data}</span></div>`
-        })
-        .join("")
-    setData(newData);
+function moveNext(setPr: (value: (((prevState: string) => string) | string)) => void, historyData: any, newSpeed: any) {
+    if (newSpeed === 0) {
+        return;
+    }
     count = count + newSpeed;
-    if (count < 0 || count >= historyData.length) {
+    if (count < 0) {
+        count = historyData.length + count;
+    } else if (count >= historyData.length) {
         count = 0;
     }
+    editor.setValue(historyData[count]);
     setPr( `${count / (historyData.length - 1) * 100}%` );
 }
 
 export const HistoryComponent = () => {
-
     const [marginPercentage, setMarginPercentage] = useState("0%")
-    const [data, setData] = useState("");
-
     const query: ParsedUrlQuery = queryString.decode(location.search, "?", "=");
     const sessionId = query.sessionId;
     historyData = getHistoryList(sessionId);
 
     useEffect( () => {
-        startPlay(setMarginPercentage, setData)
+        editor = CodeMirror(document.getElementById("editor"), options);
+        startPlay(setMarginPercentage);
+        return () => {
+            count = 0;
+            currentSpeed = 1;
+            clearInterval(timer);
+        }
     }, []);
 
     return (
-        <div className="history">
-            <div className="container code-data ">
-                <div dangerouslySetInnerHTML={{__html: data}}/>
-            </div>
+        <div className="firepad history">
+            <div id={"editor"} className={"history"} />
             <div className="panel">
                 <div className="background">
                     <div className="btns">
                         <FontAwesomeIcon color="#424350" size="lg" icon={faFastBackward} onClick={()=>{
                             currentSpeed = 0;
-                            moveNext(setData, setMarginPercentage, historyData, -1);
+                            moveNext(setMarginPercentage, historyData, -1);
                         }}/>
                         <FontAwesomeIcon color="#424350" size="lg" icon={faPlay} onClick={()=>{
                             currentSpeed = 1;
@@ -64,7 +76,7 @@ export const HistoryComponent = () => {
                         }}/>
                         <FontAwesomeIcon color="#424350" size="lg" icon={faFastForward} onClick={()=>{
                             currentSpeed = 0;
-                            moveNext(setData, setMarginPercentage, historyData, 1);
+                            moveNext(setMarginPercentage, historyData, 1);
                         }}/>
                     </div>
                     <div className="slider">
